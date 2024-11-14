399bd417cf988a1adc8565b514be45b20dafacb5
Nikhil <nikhilchouhan2002@gmail.com>
2024-11-15 01:05:05 +0530

    logger class

---
+++ src/Core/Log.cpp
@@ -0,0 +1,60 @@
+#include "Log.h"
+
+namespace Spar
+{
+    void Log::Init()
+    {
+        m_initialized = true;
+    }
+
+    void Log::Shutdown()
+    {
+        m_initialized = false;
+    }
+
+    void Log::Info(const std::string &msg)
+    {
+        LogMessage(msg, LogLevel::Info);
+    }
+
+    void Log::Warn(const std::string &msg)
+    {
+        LogMessage(msg, LogLevel::Warn);
+    }
+
+    void Log::Error(const std::string &msg)
+    {
+        LogMessage(msg, LogLevel::Error);
+    }   
+
+    void Log::LogMessage(const std::string &msg, LogLevel level)
+    {
+        if (!m_initialized)
+        {
+            return;
+        }
+
+        LogMessageData data;
+        data.message = msg;
+        data.level = level;
+
+        m_messages.push_back(data);
+
+        switch (level)
+        {
+        case LogLevel::Info:
+            std::cout << "INFO: " << msg << std::endl;
+            break; 
+        case LogLevel::Warn:
+            std::cout << "WARN: " << msg << std::endl;
+            break;
+        case LogLevel::Error:   
+            std::cout << "ERROR: " << msg << std::endl;
+            break;
+
+        default:
+            break;
+        }
+    }
+    
+};
---
+++ src/Core/Log.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "WinUtil.h"
+#include <string>
+#include <vector>
+#include <iostream>
+
+namespace Spar
+{
+    class Log
+    {
+    public:
+        static void Init();
+        static void Shutdown();
+        static void Info(const std::string &msg);
+        static void Warn(const std::string &msg);
+        static void Error(const std::string &msg);
+
+    private:
+        enum class LogLevel
+        {
+            Info,
+            Warn,
+            Error
+        };
+
+        static void LogMessage(const std::string &msg, LogLevel level);
+
+        struct LogMessageData
+        {
+            std::string message;
+            LogLevel level;
+        };
+
+    private:
+        static std::vector<LogMessageData> m_messages;
+        static bool m_initialized;
+    };
+
+};
